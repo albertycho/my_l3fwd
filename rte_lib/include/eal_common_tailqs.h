e5d2ef5258c3be26d6a572d787e7107bade863f6
albertycho90 <albertycho90@gmail.com>
2022-01-19 15:09:43 -0500

    e

--- rte_lib/include/eal_common_tailqs.h
+++ rte_lib/include/eal_common_tailqs.h
@@ -15,7 +15,7 @@
 //#include "rte_eal.h"
 #include "rte_eal_memconfig.h"
 #include "rte_per_lcore.h"
-#include "rte_lcore.h"
+//#include "rte_lcore.h"
 #include "generic/rte_atomic.h"
 #include "rte_branch_prediction.h"
 #include "rte_log.h"
