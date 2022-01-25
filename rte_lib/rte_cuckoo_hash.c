c05909529f38178fbcd792574b682d71edc2d10c
albertycho90 <albertycho90@gmail.com>
2022-01-25 11:06:39 -0500

    e

--- rte_lib/rte_cuckoo_hash.c
+++ rte_lib/rte_cuckoo_hash.c
@@ -665,7 +665,7 @@ rte_hash_add_key(const struct rte_hash* h, const void* key)
 //uint64_t
 //struct rte_hash*
 //	rte_hash_create(const struct rte_hash_parameters* params)
-int rte_hash_create(const struct rte_hash_parameters* params, struct rte_hash* ret_ptr)
+int rte_hash_create(const struct rte_hash_parameters* params, struct rte_hash** ret_ptr)
 {
 	struct rte_hash* h = NULL;
 	//struct rte_tailq_entry* te = NULL;
