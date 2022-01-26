8ee3f7dafcb5470ad268b20853ab98720523e969
Albert <albertycho90@gmail.com>
2022-01-25 20:01:45 -0500

    e

--- l3fwd/em_l3f.h
+++ l3fwd/em_l3f.h
@@ -13,7 +13,7 @@ struct rte_ipv6_hdr {
 	uint8_t  hop_limits;	/**< Hop limits. */
 	uint8_t  src_addr[16];	/**< IP address of source host. */
 	uint8_t  dst_addr[16];	/**< IP address of destination host(s). */
-};
+} __rte_packed;
 
 int em_dummy_print_func();
 
