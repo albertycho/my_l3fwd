0f583237788c229540dfc605f9bd84f85830e970
Albert <albertycho90@gmail.com>
2022-01-25 19:47:36 -0500

    e

--- l3fwd/em_l3f.c
+++ l3fwd/em_l3f.c
@@ -299,7 +299,8 @@ em_mask_key(void *key, xmm_t mask)
 	return _mm_and_si128(data, mask);
 }
 
-static inline uint16_t
+//static inline uint16_t
+uint16_t
 em_get_ipv6_dst_port(void *ipv6_hdr, uint16_t portid, void *lookup_struct)
 {
 	int ret = 0;
