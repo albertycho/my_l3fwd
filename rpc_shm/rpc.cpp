b16c60181b0f83cf1590d69da60a9ecfdffce2f6
Albert <albertycho90@gmail.com>
2022-03-04 20:03:14 -0500

    e

--- rpc_shm/rpc.cpp
+++ rpc_shm/rpc.cpp
@@ -439,6 +439,9 @@ receiveRPCRequest_zsim_l3fwd(rpcNUMAContext* rpcContext, unsigned int serv_nid, 
 
 
     if((*client_done)) {
+        if(){
+            printf("WARNING! client done recvd, but a valid packet also recvd")
+        }
         RPCWithHeader rpc;
         rpc.payload_len=0xdead;
         return rpc;
